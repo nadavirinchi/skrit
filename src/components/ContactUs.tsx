800bc86c1fa3286fabbbef819a8372249b3c1f0e
nadavirinchi <aiesec.nadavirinchi@gmail.com>
2023-07-04 16:45:03 +0530

    location changes

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -5,13 +5,12 @@ const ContactUs = () => {
   return (
     <div className='contact_us'>
         <div className='location'>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3805.4204727705646!2d78.39488497926274!3d17.48743054065848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9192334d3829%3A0x69760cc6d6ff9912!2sRd%20Number%201%2C%20Kukatpally%20Housing%20Board%20Colony%2C%20Kukatpally%2C%20Hyderabad%2C%20Telangana%20500072!5e0!3m2!1sen!2sin!4v1688367389070!5m2!1sen!2sin" width="220" height="220" style={{border:'0', borderRadius: '10px'}}  loading="lazy" ></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3887.644921061427!2d77.66927351482221!3d12.994547990840795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDU5JzQwLjQiTiA3N8KwNDAnMTcuMyJF!5e0!3m2!1sen!2sin!4v1688469065724!5m2!1sen!2sin" width="220" height="220" style={{border:'0', borderRadius: '10px'}} loading="lazy"></iframe>            
             <div style={{ display: 'flex', flexDirection: 'column', height: '100%', justifyContent: 'space-evenly', marginLeft: '1rem' }}>
                 <p className='contact_us_contact'>Contact Us</p>
-                <p className='address'>Road Number 1 <br />
-                    K P H B Phase 1, Kukatpally <br />
-                    Hyderabad, Telangana 500072 <br />
-                    India
+                <p className='address'>125, 4th Cross St, A Narayanapura,<br/>
+                 Mottappa Compound, Udaya Nagar, Mahadevapura, <br/>
+                 Bengaluru, Karnataka 560048
                 </p>
                 <div style={{ display: 'flex' }}>
                     <img style={{ width: '15px', height: '15px', marginTop: '0.15rem' }} src='/assets/whatsapp.png' alt='whatsapp' />
