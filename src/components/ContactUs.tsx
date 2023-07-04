3444f374cd57ea8f184e0439c7aceca812e77fde
nadavirinchi <aiesec.nadavirinchi@gmail.com>
2023-07-04 16:57:33 +0530

    contact us changes

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -8,13 +8,13 @@ const ContactUs = () => {
             <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3887.644921061427!2d77.66927351482221!3d12.994547990840795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDU5JzQwLjQiTiA3N8KwNDAnMTcuMyJF!5e0!3m2!1sen!2sin!4v1688469065724!5m2!1sen!2sin" width="220" height="220" style={{border:'0', borderRadius: '10px'}} loading="lazy"></iframe>            
             <div style={{ display: 'flex', flexDirection: 'column', height: '100%', justifyContent: 'space-evenly', marginLeft: '1rem' }}>
                 <p className='contact_us_contact'>Contact Us</p>
-                <p className='address'>125, 4th Cross St, A Narayanapura,<br/>
-                 Mottappa Compound, Udaya Nagar, Mahadevapura, <br/>
-                 Bengaluru, Karnataka 560048
+                <p className='address'>Second floor<br/>
+                 125, Kaveri Main Road UDAYANAGAR,<br/>
+                 Bengaluru, Karnataka 560016
                 </p>
                 <div style={{ display: 'flex' }}>
                     <img style={{ width: '15px', height: '15px', marginTop: '0.15rem' }} src='/assets/whatsapp.png' alt='whatsapp' />
-                    <p className='number'>099085 96201</p>
+                    <p className='number'>+91 960620 6893</p>
                 </div>
             </div>
         </div>
